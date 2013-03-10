4be9d21892694324ffb294a4923dbfa5b81ccf0d
a101202 <a101202@sharklasers.com>
2013-03-11 02:05:23 +0400

    pretty good architecture. need coding lexer

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,10 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
 class Lexer {
-    static List<Token> tokenList(){
-        List<Token> tokenList = new LinkedList<>();
+    private boolean isWSpace(char c) {
+        return c == 10 || c == 13 || c == ' ' || c == ';' || c == '@';
+    }
 
+    private boolean isNum(char c) {
+        return '0' <= c && c <= '9';
+    }
+
+    private boolean isAlpha(char c) {
+        return 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z';
+    }
+
+    static List<Token> tokenList(BufferedReader br) throws IOException {
+        List<Token> tokenList = new LinkedList<>();
+        String strToken; char c;
+        do{
+            c = (char)br.read();
+            strToken = "";
+        }while (c != '@');
         return null;
     }
 }
