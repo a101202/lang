c4aaf5f492e56f951051ff87db76bec1a24d3fa5
a101202 <a101202@sharklasers.com>
2013-03-19 12:02:21 +0400

    no

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,26 +1,30 @@
 import java.io.*;
 import java.util.*;
 
-class Lexer {
-    private boolean isWSpace(char c) {
-        return c == 10 || c == 13 || c == ' ' || c == ';' || c == '@';
+final class Lexer {
+    private Lexer() {}
+
+    private static boolean isWSpace(char c) {
+        return c == 10 || c == 13 || c == ' ' || c == '@';
     }
 
-    private boolean isNum(char c) {
+    private static boolean isNum(char c) {
         return '0' <= c && c <= '9';
     }
 
-    private boolean isAlpha(char c) {
+    private static boolean isAlpha(char c) {
         return 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z';
     }
 
     static List<Token> tokenList(BufferedReader br) throws IOException {
-        List<Token> tokenList = new LinkedList<>();
-        String strToken; char c;
-        do{
-            c = (char)br.read();
-            strToken = "";
-        }while (c != '@');
-        return null;
+//        List<Token> tokenList = new LinkedList<>();
+//        String strToken; char c;
+//        do{
+//            c = (char)br.read();
+//            if(isWSpace(c))
+//                continue;
+//            strToken = "";
+//        }while (c != '@');
+//        return null;
     }
 }
