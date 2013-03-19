d216b8eb51018b63170d76a1b4997fd4ec35f57c
a101202 <a101202@sharklasers.com>
2013-03-19 11:33:13 +0400

    no

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 class Lexer {
     private boolean isWSpace(char c) {
